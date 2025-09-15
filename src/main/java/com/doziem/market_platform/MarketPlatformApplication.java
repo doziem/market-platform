bcdacbc5128b730a4598972476e6fa3a4fd08602
Kenneth Chidozie <agbokennethchidoziem@gmail.com>
2025-09-15 13:54:56 +0100

    project initialized

---
+++ src/main/java/com/doziem/market_platform/MarketPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.doziem.market_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/doziem/market_platform/MarketPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doziem.market_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
